8dbf18e66ba82e8d412830e787dca58739400af0
Harish <harishprofile001@gmail.com>
2015-09-23 11:50:58 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var connect = require("connect"),
+    io = require("socket.io"),
+    chatter = require("chatter");
+
+var app = connect().use(connect.static("public")).listen(3000);
+var chat_room = io.listen(app);
+
+chatter.set_sockets(chat_room.sockets);
+
+chat_room.sockets.on("connection", function(socket) {
+    chatter.connect_chatter({
+        socket: socket,
+        name: socket.handshake.query.name
+    });
+});
