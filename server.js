62ca0a2bc3a0e5b5c757ac838ba22125bbe747a9
Harish <harishprofile001@gmail.com>
2015-09-23 15:23:48 +0530

    committed by me

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var connect = require("connect"),
     io = require("socket.io"),
     chatter = require("chatter");
 
-var app = connect().use(connect.static("public")).listen(8080);
+var app = connect().use(connect.static("public")).listen(process.env.OPENSHIFT_NODEJS_PORT || 3000, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 var chat_room = io.listen(app);
 
 chatter.set_sockets(chat_room.sockets);
